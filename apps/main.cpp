760d2e56b05b804184078578e40f080904240bf2
Janek <janskrpk@gmail.com>
2022-11-27 00:56:21 +0100

    added openlinking added basic window rendering

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,14 +1,67 @@
 #include <iostream>
+#include <memory>
 #define GLEW_STATIC
 #include "GL/glew.h"
 #include <GLFW/glfw3.h>
 #include "hello.h"
 
+const GLint WIDTH = 800, HEIGHT = 600;
+
 int main(int, char**) {
     std::string helloJim = generateHelloString("Jim");
     std::cout << helloJim << std::endl;
 
-    glewInit();
+    if(!glfwInit()){
+        std::cout << "GLFW initialization failed" << std::endl;
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+    // Get Buffer Size information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW initialisation failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Loop until window closed
+	while (!glfwWindowShouldClose(mainWindow))
+	{
+		// Get + Handle user input events
+		glfwPollEvents();
+
+		// Clear window
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
 
     return 0;
 }
